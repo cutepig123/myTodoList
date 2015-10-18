e4b975ba226a4c4c7b4a7eeb78cbecbe12a845f0
hjs <hjs>
2015-10-18 22:42:36 +0800

    Add updates

--- README.js
+++ README.js
@@ -1,11 +1,8 @@
    // code for coloring
 date	task	desc	
-10/11	//tree view with check box	F:\wamp\www\git_repos\GitHub\GeneralUtility\chrome_plugin\tree	
-		http://jsfiddle.net/TrueBlueAussie/0s0p3716/20/	
-		https://www.google.com.hk/search?newwindow=1&es_sm=122&q=javascript+treeview+with+checkbox+stack+overflow&spell=1&sa=X&ved=0CBkQBSgAahUKEwiKl52Jv7nIAhUEXqYKHYjuASU&biw=1280&bih=643	
-		//http://wwwendt.de/tech/dynatree/	
+10/11	//tree view with check box	F:\wamp\www\git_repos\GitHub\GeneralUtility\chrome_plugin\tree	http://jsfiddle.net/TrueBlueAussie/0s0p3716/20/			//https://www.google.com.hk/search?newwindow=1&es_sm=122&q=javascript+treeview+with+checkbox+stack+overflow&spell=1&sa=X&ved=0CBkQBSgAahUKEwiKl52Jv7nIAhUEXqYKHYjuASU&biw=1280&bih=643			//http://wwwendt.de/tech/dynatree/	
 		//http://stackoverflow.com/questions/10920514/using-ajax-jquery-to-refresh-an-image	$img.attr('src'
-		http://www.w3schools.com/jquery/jquery_ref_selectors.asp
+		//http://www.w3schools.com/jquery/jquery_ref_selectors.asp
 		http://www.cnblogs.com/cutepig/p/4865199.html
 		ajax attr vs prop	
 		$el.prop('checked');vs $el.is(':checked');vs $el.attr("checked");vs $el[0].checked;	https://jsperf.com/prop-vs-ischecked/5
@@ -30,19 +27,31 @@ date	task	desc
 	岗厦村租房
 	
 10/14	//Impl a task scheduler F:\wamp\www\git_repos\GitHub\GeneralUtility\TestBoost
-		还缺什么？效率不是最好，如果queue里面有多个task，而因为只有一个event，现在的做法会导致最终都会在一个thread里面执行，效率会不理想（可以参考那个event+lock+count的做法，用来跟踪queue大小，只要有task就能不停地等到event）
-		需要用户派好task顺序，以后应该支持类似于task的lib，用户乱序输入task，只要告诉互相依赖关系，其余的都在lib里面搞定了
+		//还缺什么？效率不是最好，如果queue里面有多个task，而因为只有一个event，现在的做法会导致最终都会在一个thread里面执行，效率会不理想（可以参考那个event+lock+count的做法，用来跟踪queue大小，只要有task就能不停地等到event）
+		//需要用户派好task顺序，以后应该支持类似于task的lib，用户乱序输入task，只要告诉互相依赖关系，其余的都在lib里面搞定了 (再说了)
 		
 10/16	//学习boost threadgroup的做法		http://stackoverflow.com/questions/19500404/how-to-create-a-thread-pool-using-boost-in-c
-		Synchronized Queues http://www.boost.org/doc/libs/1_59_0/doc/html/thread/sds.html
+		//Synchronized Queues http://www.boost.org/doc/libs/1_59_0/doc/html/thread/sds.html
 		
 		现在居然有这么多pattern了 https://en.wikipedia.org/wiki/Concurrency_pattern
 		
-		search from google: how to implement a thread pool学习别人的思路
-		implement a blocking queue dequeue操作会一直等待直到拿到一个task	http://stackoverflow.com/questions/20110013/implement-your-own-blocking-queue-in-java http://tutorials.jenkov.com/java-concurrency/blocking-queues.html 
-		how to implement a BlockingQueue c++
+		//search from google: how to implement a thread pool学习别人的思路
+		//implement a blocking queue dequeue操作会一直等待直到拿到一个task	http://stackoverflow.com/questions/20110013/implement-your-own-blocking-queue-in-java http://tutorials.jenkov.com/java-concurrency/blocking-queues.html 
+		//how to implement a BlockingQueue c++
 		what is monitor?
-		what is condition variable? 1) wait(scoped_lock & lock) 2) notify_one 3) notify_all http://codereview.stackexchange.com/questions/12052/implementation-of-concurrent-blocking-queue-for-producer-consumer https://msdn.microsoft.com/en-us/library/windows/desktop/ms686903(v=vs.85).aspx
+		//what is condition variable? 1) wait(scoped_lock & lock) 2) notify_one 3) notify_all http://codereview.stackexchange.com/questions/12052/implementation-of-concurrent-blocking-queue-for-producer-consumer https://msdn.microsoft.com/en-us/library/windows/desktop/ms686903(v=vs.85).aspx
 		//Java: notify() vs. notifyAll() all over again
-		http://stackoverflow.com/questions/6316286/condition-variable-alternatives-c-c-on-windows-xp
+		//http://stackoverflow.com/questions/6316286/condition-variable-alternatives-c-c-on-windows-xp
+		
+10/17	how to convert a recursive function to non-recursive? http://www.codeproject.com/Articles/418776/How-to-replace-recursive-functions-using-stack-and
+		//film: LIan Ai De Wei Dao
+		Continuation passing style (CPS) 	http://www.zhihu.com/question/20259086 https://en.wikipedia.org/wiki/Continuation-passing_style
+		CPS脱糖处理
+		原语call-with-current-continuation（通常称作call/cc） https://en.wikipedia.org/wiki/Call-with-current-continuation http://stackoverflow.com/questions/612761/what-is-call-cc
+		//film: Zhonjie zhe, X zhanjing
+		Wangyin's blog http://www.yinwang.org/
+		sicp
+10/18	DIscuss with Dapeng for the PP proj 
+		//Export and decrypt whats app data F:\WhatsApp  https://www.youtube.com/watch?v=fDEvAATH1m4 moni crypt + whatsapp viewer
+		//learn how whatsapp viewer custom control impl?
 		
