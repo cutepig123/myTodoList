ac1b496fb18b1728d942d8840b694dd42fad8abf
hjs <hjs>
2016-05-24 23:46:20 +0800

    Updates

---
+++ kindg/保 良 局 方 譚 遠 良 幼 兒 園/方譚遠良幼稚園暨幼兒園 - 幼稚園暨幼兒園服務_files/plk.js
@@ -0,0 +1,17 @@
+﻿
+//jQuery(function ($) {
+/************** start: functions. **************/
+function changelang() {
+    var temp = $(location).attr('href');
+    if (temp.indexOf("/tc/") > 0) {
+        temp = temp.replace("/tc/", "/en/");
+        window.location.href = temp;
+    }
+    else if (temp.indexOf("/en/") > 0) {
+        temp = temp.replace("/en/", "/tc/");
+        window.location.href = temp;
+    }
+}
+
+/************** end: functions. **************/
+//});  // jQuery End
