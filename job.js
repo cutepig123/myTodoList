6fa686752c18f458741e0897bf3b89af55837417
hjs <hjs>
2016-12-27 21:30:46 +0800

    update

--- job.js
+++ job.js
@@ -22,3 +22,20 @@ Google、微软等企业疯狂地刷题
 	实现JS的asyn await, promise
 	
 程序员一般通过什么途径接私活？ - 兼职- 知乎	
+
+HKPC 
+Assistant Project Officer (Ref: SMD200/16)	Engineering Business Management, Industrial/Manufacturing Engineering, Material Science or equivalent
+Associate Consultant (Ref: MCD218/16)	Engineering, Law or Business with 3 to 6 years’ relevant experience in IP services
+Senior Consultant (Ref: EMD194/16)	Environmental, Chemical, Electrical and Mechanical Engineering, and Environmental Sciences or equivalent, field of waste management / environmental management / environmental engineering
+>>Engineering Officer (Ref: APAS230/16)	Computer Science/ Electrical Engineering/ Electronics Engineering or equivalent with 3 years’ relevant experience in automotive electronics design, and electrical power electronics
+Engineering Officer (Ref: APAS229/16)	Perform 3D CAD mechanical engineering designs for automotive parts,
+R&D Manager (Ref: APAS228/16)	Lead R&D activities in automotive areas like electric vehicle relevant technology and vehicle integration
+Assistant Engineer (Intern)	Support the government-funded project on electronics system
+Undertake R&D activities such as design, analysis, development of har
+Associate Consultant (Ref: AED188/16)	disciplines such as Mechanical Engineering, Automotive Engineer
+Associate Consultant Smart Manufacturing & Materials Division Ref: SMD199/16	develop new services related to 3D CAD/CAM, robotics application
+Consultant Automotive & Electronics Division Ref: AED187/16 opto-mechatronics technology, optics design through CAE software applications
+>>Associate Consultant Automotive & Electronics Division Ref: AED181/16 Post Date : 26 October 2016 		2 years’ relevant experience
+Senior Consultant IT & Business Process Division Ref: ITD179/16 Post Date: 15 November 2016	 degree in computer science or related disciplines or equivalent with 12 years’ or more relevant experience in information security, application development or IT infrastructure management
+>>Senior Consultant Automotive & Electronics Division Ref: AED114/16 Post Date: 12 August 2016	Expert knowledge of and solid experience in at least two of the following areas: image processing, video coding,
+>>Consultant Smart Manufacturing & Materials Division Ref: MTD58N/15 Post Date: 30 August 2016
