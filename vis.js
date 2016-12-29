123e52c4e7c41b12401c928c24d8afb7a996197d
hjs <hjs>
2016-12-30 01:07:27 +0800

    update

---
+++ vis.js
@@ -0,0 +1,45 @@
+Objective:	Vision platform
+
+Modules:
+UI framework
+	Support xml based GUI designer
+	Support data binding
+	Native C++:	Qt,	GacUI
+	C# WPF
+	
+	
+Graphics framework
+	vector form 2D graphics display
+	Support 3D profile display
+	Subpixel image display
+	Export as file
+	Candidates: 
+		Qt Graphics View Framework + Qt data visualization http://doc.qt.io/qt-5/qtdatavisualization-customitems-example.html
+		Game engine?Irrlicht Engine OGRE
+		VTK
+		OpenSceneGraph
+		C# opentk	-not mature?
+		Three.js
+	
+	
+Appl framework
+	Model camera, optics, calibration record, inspection record
+	Task schecule system
+	Command dispatching system
+Alg layer	
+RAD development support
+	VS control
+	C#
+	script
+Use case
+	Load gerber file
+	Display dev file
+	align model
+	set lighting UI
+	learn UI
+	Mask editor
+	log case repeator
+	report generator
+	
+Summary
+	C# may better as UI framework, but graphics library is not so mature as C++
