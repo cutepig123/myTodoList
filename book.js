3c3895abcb598a53a1657d9c3c495a27fc1ebfd8
jshe <jinshouhe@gmail.com>
2017-09-16 19:39:34 +0800

    update

---
+++ book.js
@@ -0,0 +1,28 @@
+编程语言
+ rust，
+	https://kaisery.github.io/trpl-zh-cn/
+	https://laplacedemon.gitbooks.io/-rust/content/
+	 https://www.google.com.hk/search?newwindow=1&q=rust+%E4%B9%A6&oq=rust+%E4%B9%A6&gs_l=psy-ab.3...6418.10442.0.10585.3.3.0.0.0.0.73.203.3.3.0....0...1.1j4.64.psy-ab..0.2.135...0i67k1.juQai_9RqRw
+	 https://kaisery.gitbooks.io/rust-book-chinese/content/content/README%20%E4%BB%8B%E7%BB%8D.html
+
+C++ Primer 5th Edition (Lippman, Lajoie, and Moo) is a good book for a beginner.
+C++ Primer Plus 6th Edition (Prata) is a bad book for anyone.
+
+C#
+	CLR via C#
+	深入理解C#
+
+wpf
+	Pro WPF in C#   Matthew MacDonald著 全面而且深入，并且其实例应用性非常强。吐血推荐！５星级
+	Programming WPF》　Chris Sells and Ian Griffiths著
+	Teach Yourself  WPF in 24 Hours》 Rob Eisenberg and Christopher Bennage著 吐血推荐！５星级
+
+并发
+	Erlang/OTP并发编程实战 (豆瓣)
+	七周七并发模型 (豆瓣) （七种并发模型的比较）
+	事务信息系统  原作名: Transactional Information Systems: Theory, Algorithms, and the Practice of Concurrency Control 作者:  威肯 / 沃什 
+	事务处理 作者:  Jim Gray / Andreas Reuter 
+
+qt
+	c++ GUI Qt 4编程(第二版) 是最适合初学者的Qt官方推荐书籍
+	C++ Qt 设计模式和Qt高级编程
